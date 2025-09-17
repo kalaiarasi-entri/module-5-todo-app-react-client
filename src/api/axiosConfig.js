a9094d1699e1abe0a1f19aa4206260b3da1433d1
kalaiarasi-entri <kalaiarasientri@gmail.com>
2025-09-17 22:02:29 +0530

    url changed

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
-const api = axios.create({ baseURL: 'http://localhost:8081/api', headers:{ 'Content-Type':'application/json' } });
+const api = axios.create({ baseURL: 'http://https://module-5-todo-app-express-server.onrender.com/api', headers: { 'Content-Type': 'application/json' } });
 api.interceptors.request.use(config => { const token = localStorage.getItem('token'); if (token) config.headers.Authorization = `Bearer ${token}`; return config; });
 export default api;
